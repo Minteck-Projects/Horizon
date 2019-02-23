b30a92ba1dabf3f01715101fbead6830817125d4
Minteck <freeziv.ytb@gmail.com>
2019-02-23 15:47:26 +0000

    Mise à jour cumulative 1.5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // |  _  | (_) | |  | |/ / (_) | | | |
 // |_| |_|\___/|_|  |_/___\___/|_| |_|
 //
-// Horizon Discord Bot version 1.4.3
+// Horizon Discord Bot version 1.5
 //
 // Créé par Horizon et Minteck
 // Copyright (c) 2019, Horizon
@@ -13,46 +13,6 @@
 // Horizon est distribué sous licence Minteck Projects PLA 1.0.
 // Pour en savoir plus, vous pouvez accéder à ce site Internet :
 //    http://projectpedia.referata.com/wiki/Licence:Minteck_Projects_PLA
-//
-// ChangeLog, Mise à jour 1.5 :
-//  - Séparation des commandes en plusieurs fichiers
-//  - Création de 'libhorizon' pour un chargement plus efficace et plus sûr
-//
-// ChangeLog, Mise à jour 1.4.3 :
-//  - Finalisation du système de gestion audio
-//  - Ajout de l'option --kernel-verbose
-//
-// ChangeLog, Mise à jour 1.4.2 :
-//  - Multiples patches
-//
-// ChangeLog, Mise à jour 1.4.1 :
-//  - Amélioration des commandes ><mj et ><ml
-//  - Ajout de la commande de déboggage ><md
-//
-// ChangeLog, Mise à jour 1.4.3 :
-//  - Application du décallage horaire d'Europe Centrale au log
-//  - Ajout de l'option voiceChannel
-//
-// ChangeLog, Mise à jour 1.3.2 :
-//  - Résolution en urgence d'un problème avec la commande ><l
-//
-// ChangeLog, Mise à jour 1.3.1 :
-//  - Résolution en urgence d'un problème avec la commande ><d
-//
-// ChangeLog, Mise à jour 1.3 :
-//  - Résolution du message d'error avec fs.writeFile
-//  - Résolution du problème d'espacement du sendMessage
-//  - Ajout d'une commande pour obtenir le journal système
-//  - Ajout d'une commande pour changer le message de jeu
-//
-// ChangeLog, Mise à jour 1.2.1 :
-//  - Correction d'un problème empêchant le démarrage du bot
-//
-// ChangeLog, Mise à jour 1.2 :
-//  - Amélioration de la console (plus de détails)
-//  - Traçabilité des refus d'accès aux utilisateurs
-//  - Syntaxe globale pour la console
-//  - Capture des erreurs de communication
 
 const underscorelib = require('underscore');
 const Discord = require('discord.js');
